a30ab5b20dc4ee821885c053fbc4b941731f2e46
Blechlawine <marczinser@gmx.de>
2022-11-15 15:11:27 +0100

    prettier

--- slideshow/jquery/slideshow.js
+++ slideshow/jquery/slideshow.js
@@ -1,18 +1,18 @@
 $(".slideshow").each(function () {
-  let currentSlide = 0;
-  const images = $(this).children();
-  setInterval(function () {
+    let currentSlide = 0;
+    const images = $(this).children();
+    setInterval(function () {
+        currentSlide = changeSlide(currentSlide, images);
+    }, 5000);
     currentSlide = changeSlide(currentSlide, images);
-  }, 5000);
-  currentSlide = changeSlide(currentSlide, images);
 });
 
 function changeSlide(currentSlide, images) {
-  images.eq(currentSlide).removeClass("active");
-  currentSlide++;
-  if (currentSlide >= images.length) {
-    currentSlide = 0;
-  }
-  images.eq(currentSlide).addClass("active");
-  return currentSlide;
+    images.eq(currentSlide).removeClass("active");
+    currentSlide++;
+    if (currentSlide >= images.length) {
+        currentSlide = 0;
+    }
+    images.eq(currentSlide).addClass("active");
+    return currentSlide;
 }
